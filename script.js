582ebdf74429df89a12e553fb76bb1aa55c7e281
ashim-sharma-console <ashimsharma49@gmail.com>
2025-05-28 10:45:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-
 function convertToRoman() {
-      let num = parseInt(document.getElementById('numberInput').value);
-      if (isNaN(num) || num < 1 || num > 100000) {
-        document.getElementById('result').innerText = "Please enter a number between 1 and 100000.";
-        return;
-      }
+  const numInput = document.getElementById('numberInput').value;
+  let num = parseInt(numInput);
+  const output = document.getElementById('output');
 
-      const romanArr = [
-        ['M', 1000],
-        ['CM', 900],
-        ['D', 500],
-        ['CD', 400],
-        ['C', 100],
-        ['XC', 90],
-        ['L', 50],
-        ['XL', 40],
-        ['X', 10],
-        ['IX', 9],
-        ['V', 5],
-        ['IV', 4],
-        ['I', 1],
-      ];
+  if (isNaN(num) || num < 1 || num > 100000) {
+    output.textContent = 'Please enter a number between 1 and 100000.';
+    return;
+  }
 
-      let result = '';
-      for (let [symbol, value] of romanArr) {
-        while (num >= value) {
-          result += symbol;
-          num -= value;
-        }
-      }
+  const romanNumerals = [
+  ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
-      document.getElementById('result').innerText = result;
+  let result = '';
+  for (const [symbol, value] of romanNumerals) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
     }
+  }
+
+  output.textContent = result.toUpperCase(); // Ensures uppercase output
+}
